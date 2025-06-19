0a9375096683cd1f9e573514278896fbb3074ea7
Craig Bladow <craig@falcon.local>
2025-06-19 11:32:31 -0600

    Updated build script bbuild.sh for macOS and Linux.

--- bbuild.sh
+++ bbuild.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
-mkdir -p ./build
-gcc source/main.c -lm -o build/jtxsync
+
+# Detect OS
+ios_name="$(uname -s)"
+
+if [ "$ios_name" = "Darwin" ]; then
+    echo "Building jtxsync for macOS."
+    mkdir -p ./build
+    gcc -arch arm64 -arch x86_64 ./source/main.c  -o ./build/jtxsync
+
+elif [ "$ios_name" = "Linux" ]; then
+    echo "Building jtxsync for Linux."
+    mkdir -p ./build
+    gcc -static -s ./source/main.c -lm -o ./build/jtxsync
+
+else
+    echo "Unknown operating system: $ios_name"
+     exit 1
+fi
+
+
+
+
